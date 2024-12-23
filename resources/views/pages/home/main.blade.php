00591eb9ab59e8505e34af9eed3078ddb1999f6f
Grizerl <g397618@gmail.com>
2024-12-23 16:43:00 +0200

    top genres

--- resources/views/pages/home/main.blade.php
+++ resources/views/pages/home/main.blade.php
@@ -125,7 +125,7 @@
                             </div>
                         </div>
                         <div class="card-bottom">
-                            <h4 class="card-title">Counter-Strike: Global Offensive</h4>
+                            <h4 class="card-title">Counter-Strike 2</h4>
                             <div class="group-detaile">
                                 <div>
                                     <div class="detaile-item">
@@ -322,6 +322,257 @@
                     <h2 class="join-title">JOIN THE <span>COMMUNITY</span></h2>
                     <p class="join-lead">Join our Discord community which is in the making and made by gamers for gamers. All are we welcome to join no matter the game you play,we're here to have a good</p>
                     <button class="join-btn">Join Discord</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="sc-genres">
+    <div class="sc-sfFgg">
+        <h3 class="genres-title">Top<span>Genres</span></h3>
+        <div class="line"></div>
+        <div class="container sc-DfghF">
+            <div class="container-genres">
+                <div class="tabs-content">
+                    <ul class="list-genres">
+                        <li class="genres-item">
+                            <a href="">Action</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Indie</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Adventure</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">RPG</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Strategy</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Shooter</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Casual</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Simulation</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Puzzle</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Arcade</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Platformer</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Multiplayer</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Racing</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Sports</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Fighting</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Family</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Board Games</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Educational</a>
+                        </li>
+                        <li class="genres-item">
+                            <a href="">Card</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class-="tabs-body">
+                <div class="sc-fsfFfa">
+                <div style="margin-top: 0px;" class="card-list">
+                        <div class="sc-ktwOf card ">
+                        <div class="card-top">
+                            <img src="assets/images/games/20aa03a10cda45239fe22d035c0ebe64.jpg" alt="Grand Theft Auto V" loading="lazy">
+                            <div class="card-rating">
+                                6957
+                                <svg height="1.3em" width="1.3em" stroke="currentColor" fill="currentColor" strole-width="0" viewBox="0 0 512 512" class="st-1" xmlns="http://www.w3.org/2000/svg">
+                                    <g data-name="1" id="_1">
+                                    <path d="M370.24,425.59a14.89,14.89,0,0,1-7-1.72L257,368,150.74,423.87A15,15,0,0,1,129,408.06l20.3-118.32-86-83.8a15,15,0,0,1,8.31-25.59l118.81-17.26L243.55,55.43a15,15,0,0,1,26.9,0l53.13,107.66,118.8,17.26a15,15,0,0,1,8.32,25.59l-86,83.8L385,408.06a15,15,0,0,1-14.78,17.53ZM106,205.67l69.85,68.09A15,15,0,0,1,180.17,287l-16.49,96.14L250,337.78a15,15,0,0,1,14,0l86.34,45.39L333.83,287a15,15,0,0,1,4.31-13.27L408,205.67l-96.53-14a15,15,0,0,1-11.29-8.2L257,96l-43.17,87.47a15,15,0,0,1-11.3,8.2Z"/>
+                                    </g>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="card-bottom">
+                            <h4 class="card-title">Grand Theft Auto V</h4>
+                            <div class="group-detaile sc-grasf">
+                                <div>
+                                    <div class="detaile-item">
+                                        <p class="detaile-name">Release Date:</p>
+                                        <p class="detaile-value">2013-09-17</p>
+                                    </div>
+                                    <div class="detaile-item">
+                                        <p class="detaile-name">Updated:</p>
+                                        <p class="detaile-value">2024-12-17T10:53:09</p>
+                                    </div>
+                                </div>
+                                    <a class="detaile-btn-custom sc-sfks">See More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="sc-ktwOf card">
+                        <div class="card-top">
+                            <img src="assets/images/games/618c2031a07bbff6b4f611f10b6bcdbc.jpg" alt="The Witcher 3: Wild Hunt" loading="lazy">
+                            <div class="card-rating">
+                                6726
+                                <svg height="1.3em" width="1.3em" stroke="currentColor" fill="currentColor" strole-width="0" viewBox="0 0 512 512" class="st-1" xmlns="http://www.w3.org/2000/svg">
+                                    <g data-name="1" id="_1">
+                                    <path d="M370.24,425.59a14.89,14.89,0,0,1-7-1.72L257,368,150.74,423.87A15,15,0,0,1,129,408.06l20.3-118.32-86-83.8a15,15,0,0,1,8.31-25.59l118.81-17.26L243.55,55.43a15,15,0,0,1,26.9,0l53.13,107.66,118.8,17.26a15,15,0,0,1,8.32,25.59l-86,83.8L385,408.06a15,15,0,0,1-14.78,17.53ZM106,205.67l69.85,68.09A15,15,0,0,1,180.17,287l-16.49,96.14L250,337.78a15,15,0,0,1,14,0l86.34,45.39L333.83,287a15,15,0,0,1,4.31-13.27L408,205.67l-96.53-14a15,15,0,0,1-11.29-8.2L257,96l-43.17,87.47a15,15,0,0,1-11.3,8.2Z"/>
+                                    </g>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="card-bottom">
+                            <h4 class="card-title">The Witcher 3: Wild Hunt</h4>
+                            <div class="group-detaile sc-grasf">
+                                <div>
+                                    <div class="detaile-item">
+                                        <p class="detaile-name">Release Date:</p>
+                                        <p class="detaile-value">2015-05-18</p>
+                                    </div>
+                                    <div class="detaile-item">
+                                        <p class="detaile-name">Updated:</p>
+                                        <p class="detaile-value">2024-12-17T08:16:38</p>
+                                    </div>
+                                </div>
+                                    <a class="detaile-btn-custom sc-sfks">See More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="sc-ktwOf card">
+                        <div class="card-top">
+                            <img src="assets/images/games/2bac0e87cf45e5b508f227d281c9252a.jpg" alt="Portal 2" loading="lazy">
+                            <div class="card-rating">
+                                5773
+                                <svg height="1.3em" width="1.3em" stroke="currentColor" fill="currentColor" strole-width="0" viewBox="0 0 512 512" class="st-1" xmlns="http://www.w3.org/2000/svg">
+                                    <g data-name="1" id="_1">
+                                    <path d="M370.24,425.59a14.89,14.89,0,0,1-7-1.72L257,368,150.74,423.87A15,15,0,0,1,129,408.06l20.3-118.32-86-83.8a15,15,0,0,1,8.31-25.59l118.81-17.26L243.55,55.43a15,15,0,0,1,26.9,0l53.13,107.66,118.8,17.26a15,15,0,0,1,8.32,25.59l-86,83.8L385,408.06a15,15,0,0,1-14.78,17.53ZM106,205.67l69.85,68.09A15,15,0,0,1,180.17,287l-16.49,96.14L250,337.78a15,15,0,0,1,14,0l86.34,45.39L333.83,287a15,15,0,0,1,4.31-13.27L408,205.67l-96.53-14a15,15,0,0,1-11.29-8.2L257,96l-43.17,87.47a15,15,0,0,1-11.3,8.2Z"/>
+                                    </g>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="card-bottom">
+                            <h4 class="card-title">Portal 2</h4>
+                            <div class="group-detaile sc-grasf">
+                                <div>
+                                    <div class="detaile-item">
+                                        <p class="detaile-name">Release Date:</p>
+                                        <p class="detaile-value">2011-04-18</p>
+                                    </div>
+                                    <div class="detaile-item">
+                                        <p class="detaile-name">Updated:</p>
+                                        <p class="detaile-value">2024-12-16T19:20:51</p>
+                                    </div>
+                                </div>
+                                    <a class="detaile-btn-custom sc-sfks">See More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="sc-ktwOf card">
+                        <div class="card-top">
+                            <img src="assets/images/games/73619bd336c894d6941d926bfd563946.jpg" alt="Counter-Strike: Global Offensive" loading="lazy">
+                            <div class="card-rating">
+                                3522
+                                <svg height="1.3em" width="1.3em" stroke="currentColor" fill="currentColor" strole-width="0" viewBox="0 0 512 512" class="st-1" xmlns="http://www.w3.org/2000/svg">
+                                    <g data-name="1" id="_1">
+                                    <path d="M370.24,425.59a14.89,14.89,0,0,1-7-1.72L257,368,150.74,423.87A15,15,0,0,1,129,408.06l20.3-118.32-86-83.8a15,15,0,0,1,8.31-25.59l118.81-17.26L243.55,55.43a15,15,0,0,1,26.9,0l53.13,107.66,118.8,17.26a15,15,0,0,1,8.32,25.59l-86,83.8L385,408.06a15,15,0,0,1-14.78,17.53ZM106,205.67l69.85,68.09A15,15,0,0,1,180.17,287l-16.49,96.14L250,337.78a15,15,0,0,1,14,0l86.34,45.39L333.83,287a15,15,0,0,1,4.31-13.27L408,205.67l-96.53-14a15,15,0,0,1-11.29-8.2L257,96l-43.17,87.47a15,15,0,0,1-11.3,8.2Z"/>
+                                    </g>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="card-bottom">
+                            <h4 class="card-title">Counter-Strike 2</h4>
+                            <div class="group-detaile sc-grasf">
+                                <div>
+                                    <div class="detaile-item">
+                                        <p class="detaile-name">Release Date:</p>
+                                        <p class="detaile-value">2012-08-21</p>
+                                    </div>
+                                    <div class="detaile-item">
+                                        <p class="detaile-name">Updated:</p>
+                                        <p class="detaile-value">2024-12-16T19:19:47</p>
+                                    </div>
+                                </div>
+                                    <a class="detaile-btn-custom sc-sfks">See More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="sc-ktwOf card">
+                        <div class="card-top">
+                            <img src="assets/images/games/021c4e21a1824d2526f925eff6324653.jpg" alt="Tomb Raider (2013)" loading="lazy">
+                            <div class="card-rating">
+                                3932
+                                <svg height="1.3em" width="1.3em" stroke="currentColor" fill="currentColor" strole-width="0" viewBox="0 0 512 512" class="st-1" xmlns="http://www.w3.org/2000/svg">
+                                    <g data-name="1" id="_1">
+                                    <path d="M370.24,425.59a14.89,14.89,0,0,1-7-1.72L257,368,150.74,423.87A15,15,0,0,1,129,408.06l20.3-118.32-86-83.8a15,15,0,0,1,8.31-25.59l118.81-17.26L243.55,55.43a15,15,0,0,1,26.9,0l53.13,107.66,118.8,17.26a15,15,0,0,1,8.32,25.59l-86,83.8L385,408.06a15,15,0,0,1-14.78,17.53ZM106,205.67l69.85,68.09A15,15,0,0,1,180.17,287l-16.49,96.14L250,337.78a15,15,0,0,1,14,0l86.34,45.39L333.83,287a15,15,0,0,1,4.31-13.27L408,205.67l-96.53-14a15,15,0,0,1-11.29-8.2L257,96l-43.17,87.47a15,15,0,0,1-11.3,8.2Z"/>
+                                    </g>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="card-bottom">
+                            <h4 class="card-title">Tomb Raider (2013)</h4>
+                            <div class="group-detaile sc-grasf">
+                                <div>
+                                    <div class="detaile-item">
+                                        <p class="detaile-name">Release Date:</p>
+                                        <p class="detaile-value">2013-03-05</p>
+                                    </div>
+                                    <div class="detaile-item">
+                                        <p class="detaile-name">Updated:</p>
+                                        <p class="detaile-value">2024-12-15T13:02:43</p>
+                                    </div>
+                                </div>
+                                    <a class="detaile-btn-custom sc-sfks">See More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="sc-ktwOf card">
+                        <div class="card-top">
+                            <img src="assets/images/games/7fa0b586293c5861ee32490e953a4996.jpg" alt="Portal" loading="lazy">
+                            <div class="card-rating">
+                                4755
+                                <svg height="1.3em" width="1.3em" stroke="currentColor" fill="currentColor" strole-width="0" viewBox="0 0 512 512" class="st-1" xmlns="http://www.w3.org/2000/svg">
+                                    <g data-name="1" id="_1">
+                                    <path d="M370.24,425.59a14.89,14.89,0,0,1-7-1.72L257,368,150.74,423.87A15,15,0,0,1,129,408.06l20.3-118.32-86-83.8a15,15,0,0,1,8.31-25.59l118.81-17.26L243.55,55.43a15,15,0,0,1,26.9,0l53.13,107.66,118.8,17.26a15,15,0,0,1,8.32,25.59l-86,83.8L385,408.06a15,15,0,0,1-14.78,17.53ZM106,205.67l69.85,68.09A15,15,0,0,1,180.17,287l-16.49,96.14L250,337.78a15,15,0,0,1,14,0l86.34,45.39L333.83,287a15,15,0,0,1,4.31-13.27L408,205.67l-96.53-14a15,15,0,0,1-11.29-8.2L257,96l-43.17,87.47a15,15,0,0,1-11.3,8.2Z"/>
+                                    </g>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="card-bottom">
+                            <h4 class="card-title">Portal</h4>
+                            <div class="group-detaile sc-grasf">
+                                <div>
+                                    <div class="detaile-item">
+                                        <p class="detaile-name">Release Date:</p>
+                                        <p class="detaile-value">2007-10-09</p>
+                                    </div>
+                                    <div class="detaile-item">
+                                        <p class="detaile-name">Updated:</p>
+                                        <p class="detaile-value">2024-12-16T19:20:40</p>
+                                    </div>
+                                </div>
+                                    <a class="detaile-btn-custom sc-sfks">See More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
+                </div>
                 </div>
             </div>
         </div>
